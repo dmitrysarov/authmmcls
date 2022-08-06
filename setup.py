8e736bcc3cb14de51e3dd41beb8c9a7fb0a9d2d1
Dmitrii Victorovich Shaulskii <dmitrii.shaulskii@checklens.ai>
2022-08-06 18:54:21 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
-import codecs
-import os
-import re
-
 from setuptools import find_packages, setup
-
 
 def parse_requirements(filename):
     """load requirements from a pip requirements file"""
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
-
 def get_install_requires():
     requirements_txt = parse_requirements("requirements.txt")
     return requirements_txt
-
 
 setup(
     name="authmmcls",
